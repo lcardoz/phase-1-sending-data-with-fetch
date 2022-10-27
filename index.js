656fc99f0bce3c4354df186cdb24459743da46c5
Leah Cardoz <leahcardoz@gmail.com>
2022-10-27 17:11:33 -0400

    commit tree f1da884eec9539b951ba2940c0c0b89445f90a2a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const docBody = document.querySelector('body');
+
+function submitData() {
+	return fetch('http://localhost:3000/users', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+		},
+		body: JSON.stringify({
+			name: 'Steve',
+			email: 'steve@steve.com',
+		}),
+	})
+		.then((response) => response.json())
+		.then((users) => {
+			// console.log(users);
+			docBody.innerText = users.id;
+		})
+		.catch(function (error) {
+			alert('Something went wrong!');
+			//console.log(error.message);
+			docBody.textContent = error.message;
+		});
+}
+
+submitData();
